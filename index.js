ed66a913a768ea0b51945003a4556a4e9680def6
sahrapro208-design <sahrapro208@gmail.com>
2026-09-24 18:16:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
     ActionRowBuilder,
     ButtonBuilder,
     ButtonStyle,
+    SlashCommandBuilder,
     StringSelectMenuBuilder,
     RoleSelectMenuBuilder,
     ChannelSelectMenuBuilder,
